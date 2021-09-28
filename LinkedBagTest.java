28a7cd4e521fb74ecff8c09c222d981169bc9107
Daniel Perez <89610746+danielperez19@users.noreply.github.com>
2021-09-27 19:28:21 -0700

    Implementing union method
    Implementing the union method to test if it works (it does)
    
    (added some other comments as well)

--- LinkedBagTest.java
+++ LinkedBagTest.java
@@ -1,14 +1,16 @@
+//used the source code from the professor, so the other methods are tested and work
 import java.util.Arrays;
 
 public class LinkedBagTest 
 {
    public static void main(String[] args) 
    {
-
+      //creating two bags to compare with each other using the below methods
       BagInterface<String> bag1 = new LinkedBag<>();
 
       BagInterface<String> bag2 = new LinkedBag<>();
 
+      //adding items to both bags
       bag1.add("x");
       bag1.add("y");
       bag1.add("z");
@@ -18,6 +20,12 @@ public class LinkedBagTest
       bag2.add("y");
       bag2.add("s");
       bag2.add("m");
+      
+      System.out.println("The contents in bag1: " + Arrays.toString(bag1.toArray()));
+      System.out.println("The contents in bag2: " + Arrays.toString(bag2.toArray()));
+      
+      //implementing union method
+      System.out.println("bag1 union to bag2 is: " + Arrays.toString(bag1.union(bag2).toArray()));
 
    }
 
