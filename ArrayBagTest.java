e85361a27f81ab16a8bcbcee99a310bfcc99fa46
Daniel Perez <89610746+danielperez19@users.noreply.github.com>
2021-09-27 19:16:55 -0700

    Implement union method
    Implemented Union method in ArrayBagTest.java to see if it works

--- ArrayBagTest.java
+++ ArrayBagTest.java
@@ -8,8 +8,6 @@ public class ArrayBagTest
 
       BagInterface<String> bag2 = new ResizeableArrayBag<String>();
 
-
-
       bag1.add("x");
       bag1.add("y");
       bag1.add("z");
@@ -19,6 +17,12 @@ public class ArrayBagTest
       bag2.add("y");
       bag2.add("s");
       bag2.add("m");
-      }
+      
+      System.out.println("The contents in bag1: " + Arrays.toString(bag1.toArray()));
+      System.out.println("The contents in bag2: " + Arrays.toString(bag2.toArray()));
+      
+      //implmenting the union method
+      System.out.println("bag1 union to bag2 is: " + Arrays.toString(bag1.union(bag2).toArray()));
+   }
 
 }
