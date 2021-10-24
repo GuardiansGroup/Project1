1c26c96eadd6325a87da7f0387c5fd6d46bb48db
Daniel Perez <89610746+danielperez19@users.noreply.github.com>
2021-10-23 20:57:06 -0700

    Delete javadoc_2 directory

--- javadoc_2/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"ArrayStackTest"},{"p":"<Unnamed>","l":"ResizeableArrayStack"},{"p":"<Unnamed>","l":"StackInterface"}]
